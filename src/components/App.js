140b3b091e5c5afe6f92fa13e59a79582390ade9
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-08-19 09:41:54 +0200

    Paint Friends quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,57 @@
-// import React from "react";
 import '../styles/App.scss';
 
-// import { useState } from "react";
+import quotes from '../data/quotes.json';
+
+import {useState} from 'react';
+
 function App() {
+
+  const [quote, setQuote] =useState(quotes);
+
+  const [newQuote, setNewQuote] = useState({
+    quote:"",
+    character:"",
+  })
+
+  const renderQuotesFriends = quote.map((oneQuote, index) =>{
+    return (
+    <li key={index}>
+      <p>{oneQuote.quote} <span>-{oneQuote.character}</span></p>
+      
+    </li>)
+  });
+
+  const handleNewQuote = (event) => {
+    setNewQuote({
+      ...newQuote,
+      
+    })
+  };
+
+
+//   const[quoteFriends, setQuoteFriends] = useState("");
+
+//   useEffect(() => {
+//     fetch('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json')
+//     .then((response) => response.json())
+//     .then((responseData) => {
+//       setQuoteFriends(responseData.quoteFriends);
+//     });
+// }, []);
+
+
   return (
-    <div> <h1>Hola mundo</h1> </div>
+    <div>
+      <header>
+      <h1>Frases de Friends</h1>
+      </header>
+      
+      <main>
+        <ul>{renderQuotesFriends}</ul>
+      </main>
+  
+
+    </div>
   );
 }
 export default App;
